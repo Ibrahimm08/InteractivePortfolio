c18906d2c54c174c035ef026031e37de875bba51
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-04-15 14:37:18 +0100

    Added content

--- Script.js
+++ Script.js
@@ -4,7 +4,8 @@ function CardToggle(){
     for (let i = 0; i < block.length; i++){
         block[i].addEventListener("click", function(){
             let current = document.getElementsByClassName("large");
-            current[0].className = current[0].className.replace(" large","");
+            current[0].className = current[0].className.replace(" large"," norm");
+            this.className.replace(" norm","");
             this.className += " large";
         })
     }
